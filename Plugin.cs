6fc9689839c8f16ce0fc85a59e37dc0ae20e57e7
Itai Bergman <bergman.itai@gmail.com>
2024-01-20 21:20:06 +0200

    forgot to uncomment the Lethal Settings Register for when i was building it on my pc

--- Plugin.cs
+++ Plugin.cs
@@ -24,20 +24,24 @@ public class Plugin : BaseUnityPlugin
 
         Keybinds.Instance.AutoWalkKey.performed += AutoWalkKey_performed;
 
-        //ModMenu.RegisterMod(new ModMenu.ModSettingsConfig
-        //{
-        //    Name = GeneratedPluginInfo.Name,
-        //    Id = GeneratedPluginInfo.Identifier,
-        //    Version = GeneratedPluginInfo.Version,
-        //    Description = "Adds a rebindable key for toggling auto walk so you can give your W-key finger a break!",
-        //    MenuComponents =
-        //    [
-        //        new LabelComponent { Text = "Usage:"},
-        //        new LabelComponent { FontSize = 10, Text = "Press the bound key (\"K\" by default) to toggle auto walking on or off.\nAuto walking will automatically disable if you manually input a walking direction." },
-        //        new LabelComponent { Text = "\nRebinding Controls:", Alignment = TMPro.TextAlignmentOptions.BaselineLeft },
-        //        new LabelComponent { FontSize = 10, Text = $"To change the key binding open the settings (in game or in main menu) and click \"Change keybinds\" and look for the item labeled \"Auto Walk\".\n\n{GeneratedPluginInfo.Name} does not ship with a preset binding for controllers out of the box, but the option to set a custom controller binding is there if you want it." }
-        //    ]
-        //}, true, true);
+        if (!Enabled)
+        {
+            return;
+        }
+        ModMenu.RegisterMod(new ModMenu.ModSettingsConfig
+        {
+            Name = GeneratedPluginInfo.Name,
+            Id = GeneratedPluginInfo.Identifier,
+            Version = GeneratedPluginInfo.Version,
+            Description = "Adds a rebindable key for toggling auto walk so you can give your W-key finger a break!",
+            MenuComponents =
+            [
+                new LabelComponent { Text = "Usage:"},
+                new LabelComponent { FontSize = 10, Text = "Press the bound key (\"K\" by default) to toggle auto walking on or off.\nAuto walking will automatically disable if you manually input a walking direction." },
+                new LabelComponent { Text = "\nRebinding Controls:", Alignment = TMPro.TextAlignmentOptions.BaselineLeft },
+                new LabelComponent { FontSize = 10, Text = $"To change the key binding open the settings (in game or in main menu) and click \"Change keybinds\" and look for the item labeled \"Auto Walk\".\n\n{GeneratedPluginInfo.Name} does not ship with a preset binding for controllers out of the box, but the option to set a custom controller binding is there if you want it." }
+            ]
+        }, true, true);
     }
 
     private void AutoWalkKey_performed(InputAction.CallbackContext obj)
